9c6a833a49ea24d75653e7bab575ecc158bfbc6c
Omar Adel <omaradel1.dev@gmail.com>
2026-07-16 22:42:36 +0300

    🐛 fix(app): import settings event
    - Add missing import for settings_event required by app wiring.

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:cashier_system/features/settings/presentation/bloc/settings_event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
